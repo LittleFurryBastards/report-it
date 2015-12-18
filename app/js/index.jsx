2409b4c0fa0abe5bf1c8303da3bf4d34f0d47593
Krasimir Chariyski <kr.chariyski@gmail.com>
2015-12-18 23:25:26 +0200

    - added theming support - separate header from layout

--- app/js/index.jsx
+++ app/js/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Layout from '../components/ui/lfb-main-layout/index.jsx';
+
+// Create separated .css file
 import styles from '../css/main.scss'
 
 ReactDOM.render(
