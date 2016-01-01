e53df1526be2d0bf3dfd8dfc879020cddac0a19d
Adrian Bobev <adrian.bobev@gmail.com>
2016-01-01 11:12:50 +0200

    Add NotFound route

--- app/js/routes.jsx
+++ app/js/routes.jsx
@@ -7,12 +7,14 @@ import {Router, Route, IndexRoute} from 'react-router';
 import Layout from '../components/ui/lfb-main-layout/index.jsx';
 import Home from '../components/pages/home.jsx';
 import About from '../components/pages/about.jsx';
+import NotFound from '../components/pages/notFound.jsx';
 
 export default (
   <Router history={createBrowserHistory()}>
     <Route component={Layout} path="/">
       <IndexRoute component={Home} />
       <Route component={About} path="/about" />
+      <Route component={NotFound} path="*" />
     </Route>
   </Router>
 );
