030901691a3536d5d4305e35c327e145b7eef366
Adrian Bobev <adrian.bobev@gmail.com>
2016-01-01 11:46:04 +0200

    Fix ESLint errors

--- server.js
+++ server.js
@@ -5,42 +5,52 @@ import webpackMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import config from './webpack.config.js';
 
-const isDeveloping = process.env.NODE_ENV !== 'production';
-const port = isDeveloping ? 8080 : process.env.PORT;
-const app = express();
+/* eslint-disable no-process-env */
+const isDeveloping = process.env.NODE_ENV !== 'production',
+  port = isDeveloping ? 8080 : process.env.PORT,
+  app = express();
+
+/* eslint-enable no-process-env */
 
 if (isDeveloping) {
-  const compiler = webpack(config);
-  const middleware = webpackMiddleware(compiler, {
-    publicPath: config.output.publicPath,
-    contentBase: 'build',
-    stats: {
-      colors: true,
-      hash: false,
-      timings: true,
-      chunks: false,
-      chunkModules: false,
-      modules: false
-    }
-  });
+  const compiler = webpack(config),
+    middleware = webpackMiddleware(compiler, {
+      publicPath: config.output.publicPath,
+      contentBase: 'build',
+      stats: {
+        colors: true,
+        hash: false,
+        timings: true,
+        chunks: false,
+        chunkModules: false,
+        modules: false
+      }
+    });
 
   app.use(middleware);
   app.use(webpackHotMiddleware(compiler));
-  app.get('*', function response(req, res) {
+
+  /* eslint-disable no-sync */
+  app.get('*', (req, res) => {
     res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'build/index.html')));
     res.end();
   });
+
+  /* eslint-enable no-sync*/
 } else {
-  app.use(express.static(__dirname + '/build'));
-  app.get('*', function response(req, res) {
+  app.use(express.static(path.join(__dirname, '/build')));
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build/index.html'));
   });
 }
 
-app.listen(port, 'localhost', function onStart(err) {
+/* eslint-disable no-console */
+app.listen(port, 'localhost', (err) => {
   if (err) {
     console.log(err);
   }
 
   console.info('Open up http://localhost:%s/ in your browser.', port);
 });
+
+/* eslint-enable no-console */
