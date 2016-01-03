56e5a2f8521ff4611c13108b96f6b21c02a46926
Krasimir Chariyski <kr.chariyski@gmail.com>
2016-01-03 11:31:57 +0200

    Update navigation component:   - Add all of the needed pages   - Initial code for reusing the component for horizontal navigation

--- app/components/ui/lfb-navigation/index.jsx
+++ app/components/ui/lfb-navigation/index.jsx
@@ -8,18 +8,47 @@ import styles from './lfb-navigation.scss';
 require('./theming/lfb-navigation.scss');
 
 export default React.createClass({
+
   propTypes: {
+    isHorizontal: React.PropTypes.bool.isRequired,
+    items: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
     onNavigationItemClick: React.PropTypes.func.isRequired
   },
 
+  getDefaultProps() {
+    return {
+      isHorizontal: false,
+      items: [{home: 'Home'}, {profile: 'Profile'}, {about: 'About'}, {contacts: 'Contact us'}, {logout: 'Logout'}]
+    };
+  },
+
   render() {
+    const navigationStyle = this.props.isHorizontal ? `${styles.navigation}--is-horizontal}` : '';
+
     return (
-      <nav className={styles.navigation}>
-        <ul onClick={this.props.onNavigationItemClick}>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
+      <nav className={`${styles.navigation} ${navigationStyle}`}>
+        {this.renderItems()}
       </nav>
+    );
+  },
+
+  renderItems() {
+    return (
+      <ul onClick={this.props.onNavigationItemClick}>
+        {this.props.items.map((item) => {
+          const rout = Object.keys(item)[0],
+            routLinkText = item[rout];
+          let link;
+
+          if (rout === 'home') {
+            link = <li key={rout}><Link to={`/`}>{routLinkText}</Link></li>;
+          } else {
+            link = <li key={rout}><Link to={`/${rout}`}>{routLinkText}</Link></li>;
+          }
+
+          return link;
+        })}
+      </ul>
     );
   }
 });
--- app/js/routes.jsx
+++ app/js/routes.jsx
@@ -7,6 +7,9 @@ import {Router, Route, IndexRoute} from 'react-router';
 import Layout from '../components/ui/lfb-main-layout/index.jsx';
 import Home from '../components/pages/home.jsx';
 import About from '../components/pages/about.jsx';
+import Contacts from '../components/pages/contacts.jsx';
+import Logout from '../components/pages/logout.jsx';
+import Profile from '../components/pages/profile.jsx';
 import NotFound from '../components/pages/notFound.jsx';
 
 export default (
@@ -14,6 +17,9 @@ export default (
     <Route component={Layout} path="/">
       <IndexRoute component={Home} />
       <Route component={About} path="/about" />
+      <Route component={Contacts} path="/contacts" />
+      <Route component={Logout} path="/logout" />
+      <Route component={Profile} path="/profile" />
       <Route component={NotFound} path="*" />
     </Route>
   </Router>
