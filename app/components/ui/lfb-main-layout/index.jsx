50360b8560ba7d7ec0d86a716df91dd81a479b4b
Krasimir Chariyski <kr.chariyski@gmail.com>
2016-01-06 15:11:41 +0200

    Added navigation to issue page

--- app/components/ui/lfb-main-layout/index.jsx
+++ app/components/ui/lfb-main-layout/index.jsx
@@ -1,6 +1,7 @@
 'use strict';
 
 import React from 'react';
+import {Link} from 'react-router';
 import Navigation from '../lfb-navigation/index.jsx';
 
 // Styles
@@ -18,6 +19,14 @@ export default React.createClass({
     };
   },
 
+  componentWillMount(nextState) {
+    this._setShowButtonState(nextState);
+  },
+
+  componentWillReceiveProps() {
+    this._setShowButtonState();
+  },
+
   render() {
     const asideIsVisible = this.state.isAsideVisible ? `${styles.aside}--is-visible` : '',
       overlayIsVisible = this.state.isAsideVisible ? `${styles.overlay}--is-visible` : '';
@@ -26,6 +35,7 @@ export default React.createClass({
       <main className={styles.wrapper}>
         <header className={styles.header}>
           <button onClick={this.toggleAside}>menu</button>
+          {this._renderHeaderNavigationLink()}
         </header>
 
         <article className={styles.content}>{this.props.children}</article>
@@ -63,5 +73,24 @@ export default React.createClass({
       {icon: 'plus32', rout: 'profile', routLinkText: 'Profile'},
       {icon: 'plus32', rout: 'logout', routLinkText: 'Logout'}
     ];
+  },
+
+  _renderHeaderNavigationLink() {
+    if (this.state.showBackButton) {
+      return (<a href="#" onClick={function (event) {
+        event.preventDefault();
+        window.history.go(-1);
+      }}>Cancel X</a>);
+    } else {
+      return <Link to={"/issue"}><span>Create Issue +</span></Link>;
+    }
+  },
+
+  _setShowButtonState() {
+    if (window.location.pathname.indexOf('issue') === -1) {
+      this.setState({showBackButton: false});
+    } else {
+      this.setState({showBackButton: true});
+    }
   }
 });
